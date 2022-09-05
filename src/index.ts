54859d2c28e27e843124f222fdde773148277bc7
jaks024 <ye7748560@gmail.com>
2022-09-05 00:25:46 -0400

    added new endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,51 @@
 import express from "express";
 import { DriveController } from "./DriveController";
+
 const cors = require('cors');
 const app = express();
 const driveController = DriveController();
+
 const port = 3000;
+const accessTokenHeaderName = "accessToken";
+const refreshTokenHeaderName = "refreshToken";
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("test!!!!!");
+  res.send("Backend for ExpV2");
 });
 
+// get refresh token
 app.get("/auth/google", async (req, res) => {
   const tokens = await driveController.GetRefreshToken(req.header("code"));
-
   res.json(tokens);
 });
 
+// get access token
 app.get("/auth/access", async (req, res) => {
-  const tokens = await driveController.GetAccessToken(req.header("refreshToken"));
-
+  const tokens = await driveController.GetAccessToken(req.header(refreshTokenHeaderName));
   res.json(tokens);
 });
 
-app.get("/list", (req, res) => {
-  res.json(driveController.ListFiles(req.header("AccessToken")));
+// get computed summary data for <year> <month> 
+app.get("/summary/:year-:month", (req, res) => {
+  
+  res.send(`${req.params.month} - ${req.params.year}`);
+  res.json();
+}); 
+
+// get all entires for <year> <month> 
+app.get("/history/:year-:month", (req, res) => {
+
 });
 
-app.get("/summary/:auth-:year-:month", (req, res) => {
-  res.send(`${req.params.month} - ${req.params.year} | user auth ${req.params.auth}`);
-}); 
-
-app.post("/summary", (req, res) => {
-  res.json({ requestBody: req.body });
-}); 
+// add new entry
+app.post("/add", async (req, res) => {
+  await driveController.SaveFile(req.header(accessTokenHeaderName), req.body);
+});
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`server is running at http://localhost:${port}`);
 });
